36d95d17d3dc2e9eac553b9827bd159581475ad3
Tom Selfin <selfint@gmail.com>
2021-01-02 12:40:25 +0200

    change to layer based architecture

--- src/network.rs
+++ src/network.rs
@@ -2,14 +2,31 @@ use ndarray::prelude::*;
 use ndarray_rand::rand_distr::Uniform;
 use ndarray_rand::RandomExt;
 
-pub struct Network {}
+pub struct FullyConnected {
+    size: usize,
+    weights: Option<Array2<f32>>,
+    biases: Option<Array1<f32>>,
+    input: Option<Box<FullyConnected>>,
+}
 
-impl Network {
-    fn generate_weights(dims: &[usize]) -> Vec<Array2<f32>> {
-        dims.iter()
-            .zip(dims.iter().skip(1))
-            .map(|(&layer, &next)| Array2::random((next, layer), Uniform::new(-0.01, 0.01)))
-            .collect()
+impl FullyConnected {
+    fn new(size: usize) -> Self {
+        FullyConnected {
+            size,
+            weights: None,
+            biases: None,
+            input: None,
+        }
+    }
+
+    fn stack(input: FullyConnected, size: usize) -> Self {
+        let distribution = Uniform::new(-0.01, 0.01);
+        FullyConnected {
+            size,
+            weights: Some(Array2::random((size, input.size), distribution)),
+            biases: Some(Array1::random(size, distribution)),
+            input: Some(Box::new(input)),
+        }
     }
 }
 
@@ -18,12 +35,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_generate_weights() {
-        let weights: Vec<Array2<f32>> = Network::generate_weights(&[2, 3, 1]);
+    fn test_stack_layers() {
+        let input = FullyConnected::new(2);
+        let hidden = FullyConnected::stack(input, 3);
 
-        assert_eq!(weights.len(), 2);
+        assert!(hidden.weights.is_some());
+        assert!(hidden.biases.is_some());
+        assert_eq!(hidden.weights.clone().unwrap().len(), 6);
+        assert_eq!(hidden.biases.clone().unwrap().len(), 3);
 
-        assert_eq!(weights[0].len(), 6);
-        assert_eq!(weights[1].len(), 3);
+        let output = FullyConnected::stack(hidden, 1);
+
+        assert!(output.weights.is_some());
+        assert!(output.biases.is_some());
+        assert_eq!(output.weights.unwrap().len(), 3);
+        assert_eq!(output.biases.unwrap().len(), 1);
     }
 }
