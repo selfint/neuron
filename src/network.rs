66561d8c1100c0b1bb9e031e3abf8235bfd53c80
Tom Selfin <selfint@gmail.com>
2021-01-02 10:45:09 +0200

    add generate weights

--- src/network.rs
+++ src/network.rs
@@ -1,8 +1,15 @@
+use ndarray::prelude::*;
+use ndarray_rand::rand_distr::Uniform;
+use ndarray_rand::RandomExt;
+
 pub struct Network {}
 
 impl Network {
-    pub fn new(dims: &[usize]) -> Self {
-        Network
+    fn generate_weights(dims: &[usize]) -> Vec<Array2<f32>> {
+        dims.iter()
+            .zip(dims.iter().skip(1))
+            .map(|(&layer, &next)| Array2::random((next, layer), Uniform::new(-0.01, 0.01)))
+            .collect()
     }
 }
 
@@ -11,9 +18,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_weights_and_biases() {
-        let network = Network::new(&[2, 3, 1]);
+    fn test_generate_weights() {
+        let weights: Vec<Array2<f32>> = Network::generate_weights(&[2, 3, 1]);
 
-        assert_eq!(network.weights.len(), 3);
+        assert_eq!(weights.len(), 2);
+
+        assert_eq!(weights[0].len(), 6);
+        assert_eq!(weights[1].len(), 3);
     }
 }
